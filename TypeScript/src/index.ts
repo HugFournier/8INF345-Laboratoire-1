f295cd971026227435f80f8f9481cf49e4a11ed6
Thomas <tomza@live.fr>
2018-02-10 16:37:21 -0500

    recup url

--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -5,28 +5,32 @@ import AccueilController from './Controller/AccueilController';
 import { url } from 'inspector';
 
 $(function(){
+    let uri = parseQueryString("");
+    console.log(uri);
     let accueilController = new AccueilController();
     let body = $('body');
     let htmlAcc = "";
     htmlAcc += accueilController.getNav();
     htmlAcc += accueilController.getArticles();
     body.html(htmlAcc);
-    let uri = parseURL("");
-    console.log(uri);
 });
 
-function parseURL(stringUrl: string) : Map<string, string> {
-    if(stringUrl == null){
-        stringUrl = window.location.search.substring(1);
+//Récupère les paramètres dans l'URL
+function parseQueryString(queryString: string): Map<string, string> {
+    if (queryString == null) {
+        queryString = window.location.search.substring(1);
     }
 
     var params = new Map<string, string>();
-    var queries = stringUrl.split("&");
+
+    var queries = queryString.split("&");
 
     queries.forEach((indexQuery: string) => {
         var indexPair = indexQuery.split("=");
+
         var queryKey = decodeURIComponent(indexPair[0]);
         var queryValue = decodeURIComponent(indexPair.length > 1 ? indexPair[1] : "");
+
         params[queryKey] = queryValue;
     });
 
