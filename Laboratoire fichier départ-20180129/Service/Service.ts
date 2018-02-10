7be9f5a5230c0c381edfffc9c7104712deb1865f
Thomas <tomza@live.fr>
2018-02-10 00:20:12 -0500

    delete ancien fichier

--- Laboratoire fichier départ-20180129/Service/Service.ts
+++
@@ -1,8 +0,0 @@
-class Service {
-    public static returnArticlesStub() : Array<Article> {
-        return [
-            new Article("Toto","blablabla","http://lorempixel.com/200/200"),
-            new Article("Tata","blobloblo","http://lorempixel.com/200/200"),            
-        ];
-    }
-}
